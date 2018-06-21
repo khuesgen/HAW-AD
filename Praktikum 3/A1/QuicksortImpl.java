87cf1d6cc0fa06b34201b262f45327480b953ef7
Hüsgen <Kevin.Huesgen@haw-hamburg.de>
2018-06-21 19:34:27 +0200

    Quicksort angefangen

--- Praktikum 3/A1/QuicksortImpl.java
+++ Praktikum 3/A1/QuicksortImpl.java
@@ -17,9 +17,41 @@ public class QuicksortImpl<T extends Comparable<T>, U> extends Quicksort<T,U> {
 
 
 	private void quicksort(SchluesselWertPaar<T, U>[] a, int links, int rechts) {
-		// TODO Auto-generated method stub
+		if (links < rechts) {
+			int pivotElement = pivotStrategy.getIndex(a, links, rechts);
+			int i = partition(a, links, rechts, pivotElement);
+			quicksort(a, links , i-1);
+			quicksort(a, i +1 , rechts);
+		}
 		
 	}
-
 	
+	// https://de.wikiversity.org/wiki/Kurs:Algorithmen_und_Datenstrukturen/Vorlesung/QuickSort#Pivot_Element
+	private int partition(SchluesselWertPaar<T, U>[] a, int links, int rechts, int pivotElement) {
+		T pivot = a[pivotElement].getSchluessel();
+		System.out.println("Pivotelement: " + pivot);
+		while (links < rechts) {
+			
+			while (a[links].getSchluessel().compareTo(pivot) < 0) {
+				System.out.println("Erhöhe Links");
+				links++;
+			} 
+			while (a[rechts].getSchluessel().compareTo(pivot) > 0) {
+				System.out.println("Verringere Rechts");
+				rechts--;
+			} 
+			
+			if (links < rechts) {
+				System.out.println("Tausche");
+				SchluesselWertPaar<T, U> temp = a[links];
+				a[links] = a[rechts];
+				a[rechts] = temp;
+			}
+			
+
+		
+		}
+		return links;
+	}
+		
 }
