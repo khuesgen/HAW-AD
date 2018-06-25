8ef9afbdbe535f5e2a44863c0ae1ff9c671467a2
DESKTOP-QCG35DD\Flo <florian.haverkamp@haw-hamburg.de>
2018-06-25 11:59:43 +0200

    BBaum weitergearbeitet

--- Praktikum 3/A2/BBaumLoesung.java
+++ Praktikum 3/A2/BBaumLoesung.java
@@ -1,21 +1,42 @@
 package A2;
 
-public class BBaumLoesung <T extends Comparable<T>, U> extends BBaum<T, U> {
+public class BBaumLoesung<T extends Comparable<T>, U> extends BBaum<T, U> {
 
 	public BBaumLoesung(int ordnung) {
 		super(ordnung);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void einfuegen(T schluessel, U wert) {
 		SchluesselWertPaar<T, U> element = new SchluesselWertPaar<T, U>(schluessel, wert);
-		
+
 		if (wurzel == null) {
 			wurzel = new BBaumKnoten<T, U>(element);
 		} else {
-			//TODO
+			einfügenrekursiv(element, wurzel);
 		}
+	}
+
+	public void einfügenrekursiv(SchluesselWertPaar<T, U> element, BBaumKnoten<T, U> knoten) {
+
+		if (knoten.istBlattknoten()) {
+			// TODO
+
+		} else {
+
+			for (int i = 0; i < knoten.getAnzahlSchluesselWertPaare(); i++) {
+
+				if (element.getSchluessel().compareTo(knoten.getSchluesselWertPaar(i).getSchluessel()) == -1) {
+					einfügenrekursiv(element, knoten.getKind(i));
+					break;
+
+				} else if (i + 1 == knoten.getAnzahlSchluesselWertPaare()) {
+					einfügenrekursiv(element, knoten.getKind(i + 1));
+				}
+			}
+
+		}
+
 	}
 
 }
